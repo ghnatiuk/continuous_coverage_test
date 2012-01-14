3f8d35e64681ad8a80c278711d7bf79c5b58519d
Gregory Hnatiuk <ghnatiuk@gmail.com>
2012-01-13 22:05:36 -0500

    Remove included headers, specify them when creating Makefile

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,2 +1,29 @@
 require 'mkmf'
+
+dir_config('ruby', [],[]) # no defaults .. use --with-ruby-include
+
+unless find_header("atomic.h")
+  abort "atomic.h is missing.  Supply path to ruby 1.9.3 source w/ --with-ruby-include=\"<path>\""
+end
+unless find_header("debug.h")
+  abort "debug.h is missing"
+end
+unless find_header("id.h")
+  abort "id.h is missing"
+end
+unless find_header("method.h")
+  abort "method.h is missing"
+end
+unless find_header("node.h")
+  abort "node.h is missing"
+end
+unless find_header("thread_pthread.h")
+  abort "thread_pthread.h is missing"
+end
+unless find_header("vm_core.h")
+  abort "vm_core.h is missing"
+end
+unless find_header("vm_opts.h")
+  abort "vm_opts.h is missing"
+end
 create_makefile('ccoverage')
